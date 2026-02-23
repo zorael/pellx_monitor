eca1eaa0d3870119c2c84ba802a1dc3197c1fc9c
JR <zorael@gmail.com>
2026-02-23 23:22:54 +0100

    Don't publically import state::LevelNotifier

--- src/notify/mod.rs
+++ src/notify/mod.rs
@@ -6,6 +6,7 @@ pub mod two_level;
 
 pub use context::Context;
 pub use result::NotificationResult;
-pub use state::LevelNotifier;
 pub use traits::Notifier;
 pub use two_level::TwoLevelNotifier;
+
+use state::LevelNotifier;
