06d5c33dbc8131af5116f270f475f5765b0d35d0
JR <zorael@gmail.com>
2026-02-23 23:52:22 +0100

    Include what email a BatsignBackend sends to in its name()

--- src/backend/batsign.rs
+++ src/backend/batsign.rs
@@ -29,7 +29,11 @@ impl super::Backend for BatsignBackend {
     /// Returns the name of the backend, which is "batsign" in this case.
     fn name(&self) -> String {
         // This can be cached if it turns out to be a hotspot.
-        format!("batsign#{}", self.id)
+        format!(
+            "batsign#{} ({})",
+            self.id,
+            get_email_from_batsign_url(&self.url).unwrap_or("unknown email")
+        )
     }
 
     /// Builds the message to be sent via Batsign.
@@ -47,20 +51,8 @@ impl super::Backend for BatsignBackend {
     }
 }
 
-/// Extracts email addresses from a list of Batsign URLs, returning them as a comma-separated string.
-#[cfg(false)]
-fn get_emails_from_batsign_urls(urls: &[String]) -> Option<String> {
-    let emails: Vec<&str> = urls
-        .iter()
-        .filter_map(|u| get_email_from_single_batsign_url(u))
-        .collect();
-
-    (!emails.is_empty()).then_some(emails.join(", "))
-}
-
 /// Extracts an email address from a single Batsign URL, returning it as a `&str`.
-#[cfg(false)]
-fn get_email_from_single_batsign_url(url: &str) -> Option<&str> {
+fn get_email_from_batsign_url(url: &str) -> Option<&str> {
     // https://batsign.me/at/{email}/{token}
     //       ^^          ^  ^       ^       ^?
     let mut parts = url.split('/');
@@ -75,44 +67,28 @@ fn get_email_from_single_batsign_url(url: &str) -> Option<&str> {
     None
 }
 
-#[cfg(false)]
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_get_emails_from_batsign_urls() {
-        let vec = vec![
-            "https://batsign.me/at/test@example.com/token".to_string(),
-            "https://batsign.me/at/example@test.com/token".to_string(),
-            "https://batsign.me/at/blork/token".to_string(),
-            "https://batsign.me/".to_string(),
-            "".to_string(),
-        ];
-
-        let expected = Some("test@example.com, example@test.com".to_string());
-        let emails = super::get_emails_from_batsign_urls(&vec);
-        assert_eq!(emails, expected);
-    }
-
-    #[test]
-    fn test_get_email_from_single_batsign_url() {
+    fn test_get_email_from_batsign_url() {
         let url = "https://batsign.me/at/test@example.com/token";
-        let email = super::get_email_from_single_batsign_url(url);
+        let email = super::get_email_from_batsign_url(url);
         assert_eq!(email, Some("test@example.com"));
 
         let url = "https://batsign.me/at/example@test.com/token";
-        let email = super::get_email_from_single_batsign_url(url);
+        let email = super::get_email_from_batsign_url(url);
         assert_eq!(email, Some("example@test.com"));
 
         let url = "https://batsign.me/at/blork/token";
-        let email = super::get_email_from_single_batsign_url(url);
+        let email = super::get_email_from_batsign_url(url);
         assert_eq!(email, None);
 
         let url = "https://batsign.me/";
-        let email = super::get_email_from_single_batsign_url(url);
+        let email = super::get_email_from_batsign_url(url);
         assert_eq!(email, None);
 
         let url = "";
-        let email = super::get_email_from_single_batsign_url(url);
+        let email = super::get_email_from_batsign_url(url);
         assert_eq!(email, None);
     }
 }
